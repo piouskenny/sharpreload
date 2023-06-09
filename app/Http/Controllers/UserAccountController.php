ad0064182bfdd9c85ca0cf9557feabdde35e0fef
piouskenny <kehindeu13@gmail.com>
2023-06-09 16:13:10 +0100

    deposit_request services implemented

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -64,7 +64,6 @@ class UserAccountController extends Controller
         );
     }
 
-
     public function deposit_request(Request $request)
     {
         $request->validate(
@@ -73,58 +72,9 @@ class UserAccountController extends Controller
                 'pin' => 'required|integer|min:4'
             ]
         );
-
-
-        $user = User::where('id', '=', session('User'))->first();
-
-        if ($user->pin != $request->pin) {
-            return back()->with('failed', 'incorrect user pin');
-        }
-        $amount_to_deposite = $request->amount;
-
-        $amount_to_deposited = $request->amount + $request->old_balance;
-
-        if ($amount_to_deposite < 100 || $amount_to_deposite > 50000) {
-            // This is the code that saves the transcation History if the amount is not valid
-            // I will hard code it for now and implent the form later
-            $transaction_history = TransactionHistory::create(
-                [
-                    'user_id' => $user->id,
-                    'transaction_type' => "Credit",
-                    'payment_method' => "Bank Transfer",
-                    'amount' => $amount_to_deposite,
-                    'status' => "failed"
-                ]
-            );
-            return back()->with('failed', "#$amount_to_deposite is an invalid amount minimum deposit is #100 and Maximunm is #50,000");
-        }
-
-        $amount = UserAccount::where('user_id', $user->id)->update([
-            'account_balance' => $amount_to_deposited
-        ]);
-
-
-        if ($amount) {
-            // This is the code that saves the transcation History if it successed
-
-            // I will hard code it for now and implent the form later
-            if ($amount_to_deposite >= 100) {
-                $transaction_history = TransactionHistory::create(
-                    [
-                        'user_id' => $user->id,
-                        'transaction_type' => "Credit",
-                        'payment_method' => "Bank Transfer",
-                        'amount' => $amount_to_deposite,
-                        'status' => "success"
-                    ]
-                );
-            }
-
-            return back()->with('message', "$amount_to_deposite was deposited to your account successfully");
-        } else {
-
-            return back()->with('message_error', "The deposit of $amount_to_deposite to your account failed");
-        }
+        $this->UserAccountControllerServices = new UserAccountControllerServices;
+        $output = $this->UserAccountControllerServices->deposit_request($request);
+        return $output;
     }
 
 
--- app/Services/UserAccountControllerServices.php
+++ app/Services/UserAccountControllerServices.php
@@ -61,7 +61,64 @@ class UserAccountControllerServices
         if (!$user->pin) {
             return view('User.pin_page')->with('user', $user);
         }
-        
+
+        return $output;
+    }
+
+    public function deposit_request($request)
+    {
+        $user = User::where('id', '=', session('User'))->first();
+
+        if ($user->pin != $request->pin) {
+            return back()->with('failed', 'incorrect user pin');
+        }
+
+        $amount_to_deposite = $request->amount;
+
+        $amount_to_deposited = $request->amount + $request->old_balance;
+
+        if ($amount_to_deposite < 100 || $amount_to_deposite > 50000) {
+            // This is the code that saves the transcation History if the amount is not valid
+            // I will hard code it for now and implent the form later
+            $transaction_history = TransactionHistory::create(
+                [
+                    'user_id' => $user->id,
+                    'transaction_type' => "Credit",
+                    'payment_method' => "Bank Transfer",
+                    'amount' => $amount_to_deposite,
+                    'status' => "failed"
+                ]
+            );
+            return back()->with('failed', "#$amount_to_deposite is an invalid amount minimum deposit is #100 and Maximunm is #50,000");
+        }
+
+        $amount = UserAccount::where('user_id', $user->id)->update([
+            'account_balance' => $amount_to_deposited
+        ]);
+
+
+        if ($amount) {
+            // This is the code that saves the transcation History if it successed
+
+            // I will hard code it for now and implent the form later
+            if ($amount_to_deposite >= 100) {
+                $transaction_history = TransactionHistory::create(
+                    [
+                        'user_id' => $user->id,
+                        'transaction_type' => "Credit",
+                        'payment_method' => "Bank Transfer",
+                        'amount' => $amount_to_deposite,
+                        'status' => "success"
+                    ]
+                );
+            }
+
+            $output =  back()->with('message', "$amount_to_deposite was deposited to your account successfully");
+        } else {
+
+            $output = back()->with('message_error', "The deposit of $amount_to_deposite to your account failed");
+        }
+
         return $output;
     }
 }
